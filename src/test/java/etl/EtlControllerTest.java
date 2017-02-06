1eb57c92473a77ea4500ae122908f053e777fb28
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-02-06 10:12:05 +0300

    Fixture reuse demo.

--- src/test/java/etl/EtlControllerTest.java
+++ src/test/java/etl/EtlControllerTest.java
@@ -3,34 +3,57 @@ package etl;
 import etl.domain.*;
 import etl.extractor.CsvFormatException;
 import etl.extractor.IoProblemExtractor;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
 
 public class EtlControllerTest {
-//    @Test(expected = EtlException.class, timeout = 1_000)
-    public void shouldDoEtlOperationWhenNoIoProblem() throws EtlException, CsvFormatException {
+    private EtlController etlController;
+    private Loader loaderMock;
+    private User[] usersDummy;
+
+    @Before
+    public void prepare() throws CsvFormatException {
         //region Given | Fixture
         final Extractor extractorStub = mock(Extractor.class);
-        final User[] usersDummy = {
+        usersDummy = new User[]{
             new User(1, "user01"),
             new User(2, "user02")
         };
         when(extractorStub.extract()).thenReturn(usersDummy);
 
-        final Loader loaderMock = mock(Loader.class);
-        final EtlController etlController = new EtlController(
+        this.loaderMock = mock(Loader.class);
+        this.etlController = new EtlController(
             extractorStub,
             new Loader[] { loaderMock }
         );
         //endregion
+    }
 
+//    @Test(expected = EtlException.class, timeout = 1_000)
+    public void shouldDoEtlOperationWhenNoIoProblem() throws EtlException, CsvFormatException {
         //region When
-        etlController.doEtl();
+        this.etlController.doEtl();
         //endregion
 
         //region Then
         verify(loaderMock, times(1)).load(usersDummy);
         //endregion
+    }
+
+    public void fixtureBuilderDemo() {
+        ExtractorStubBuilder
+            .withDataSource(
+                DataSourceStubBuilder
+                    .withDataRecord("...")
+                    .withDataRecord("...")
+                    .withDataRecord("...")
+                    .withDataRecord("...")
+                    .withDataRecord("...")
+                    .withDataRecord("...")
+                .build())
+            .withStringFormat("....")
+            .build();
     }
 }
